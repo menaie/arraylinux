77594abcc865a57940da8e079d15b522fbd7b035
menaie <edumenai98@gmail.com>
2024-12-11 13:26:21 +0000

    a bunch of things

---
+++ arraylinux-archiso/airootfs/root/.calkde.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm /usr/share/applications/calamares.desktop
+mv /usr/share/applications/calamares1.desktop /usr/share/applications/calamares.desktop
+chmod +x /home/*/Desktop/Install_System.desktop
+
