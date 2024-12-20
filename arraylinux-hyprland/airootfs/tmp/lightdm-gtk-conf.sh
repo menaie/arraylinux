44266490b0f53c43ecd5e67e8ba3934eb2b063b7
menaie <edumenai98@gmail.com>
2024-12-20 10:48:55 -0300

    really a lot of things

--- arraylinux-hyprland/airootfs/tmp/lightdm-gtk-conf.sh
+++ arraylinux-hyprland/airootfs/tmp/lightdm-gtk-conf.sh
@@ -7,3 +7,5 @@ cp /usr/share/pixmaps/arraylinux-logo.png /mnt/usr/share/pixmaps/arraylinux-logo
 cp /usr/share/pixmaps/arraylinux-wallpaper.png /mnt/usr/share/pixmaps/arraylinux-wallpaper.png
 
 cp /usr/share/wallpapers/arraylinux-wallpaper.png /mnt/usr/share/wallpapers/arraylinux-wallpaper.png
+
+cp -r /usr/share/themes/arraylinux-theme /mnt/usr/share/themes/arraylinux-theme
